bc767602be22cf852055f2fb606f1f9e11ccef87
Martin Biermann <info@martinbiermann.com>
2014-01-27 16:52:30 +0100

    Adds user reference to graph installation execution in graph recipe

--- recipes/graph.rb
+++ recipes/graph.rb
@@ -3,9 +3,12 @@ file = "rexster-cassandra-es.xml"
 bash "install_graph" do
 	code <<-EOH
 		mv #{file} #{file}.old
-		sed "s/<graph-name>[^<]*<\/graph-name>/<graph-name>#{node[:titan][:graph]}<\/graph-name>/"  #{file}.old > #{file}
+		sed "s/<graph-name>[^<]*<\/graph-name>/<graph-name>#{node[:titan][:graph]}<\/graph-name>/" #{file}.old > #{file}
 		rm -f #{file}.old
+		chmod 0755 #{file}
 	EOH
+	user	"titan"
+	group	"titan"
 	cwd "/usr/local/titan/conf/"
 	notifies :restart, 'service[titan]', :delayed
 end
