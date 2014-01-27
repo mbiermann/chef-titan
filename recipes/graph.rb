6132a265855cb897d2e059abcb3ce0fe126c6314
Martin Biermann <info@martinbiermann.com>
2014-01-27 14:32:19 +0100

    Fixes typo in sed command in graph recipe

--- recipes/graph.rb
+++ recipes/graph.rb
@@ -3,7 +3,7 @@ file = "rexster-cassandra-es.xml"
 bash "install_graph" do
 	code <<-EOH
 		mv #{file} #{file}.old
-		sed "s/<graph-name>[^<]*</graph-name>/<graph-name>#{node[:titan][:graph]}</graph-name>/"  #{file}.old > #{file}
+		sed "s/<graph-name>[^<]*<\/graph-name>/<graph-name>#{node[:titan][:graph]}<\/graph-name>/"  #{file}.old > #{file}
 		rm -f #{file}.old
 	EOH
 	cwd "/usr/local/titan/conf/"
